8da1a40831c9c11d6ae98d608774e008d4afffc5
shuja <shuja.devops@gmail.com>
2021-12-29 21:36:50 +0530

    within dir create job for cart component

--- seed.groovy
+++ seed.groovy
@@ -18,3 +18,31 @@ pipelineJob('roboshop-ansible') {
         }
     }
 }
+folder('CI-Pipelines') {
+    displayName('CI-Pipelines')
+    description('CI-Pipelines')
+}
+pipelineJob('CI-Pipelines/cart') {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/shuja-git/cart.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('*/main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+
+
+
+
